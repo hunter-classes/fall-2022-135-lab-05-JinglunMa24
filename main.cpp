fd6b6bc6d0ecb49a6ada22460f6ed1a739b8c106
JinglunMa24 <2461447099ma@gmail.com>
2022-10-06 11:52:49 -0400

    added main files

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 
 #include "funcs.h"
 
-int main()
-{
-  return 0;
+int main() {
+
+	std::cout << "isDivisibleBy(100, 25): " << isDivisibleBy(100, 25) << std::endl;
+
+	std::cout << "isPrime(11): " << isPrime(11) << std::endl;
+
+	std::cout << "nextPrime(20): " << nextPrime(20) << std::endl;
+
+	std::cout << "countPrimes(4, 40): " << countPrimes(4, 40) << std::endl;
+
+	std::cout << "isTwinPrime(29): " << isTwinPrime(29) << std::endl;
+
+	std::cout << "nextTwinPrime(19): " << nextTwinPrime(19) << std::endl;
+
+	std::cout << "largestTwinPrime(1, 31): " << largestTwinPrime(1, 31) << std::endl;
+
+	return 0;
 }
